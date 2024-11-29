c807016710a0bd60d18ab0ec27e32ceeae8f9d49
Nathaniel <antonhillarynathaniel@gmail.com>
2024-11-29 14:23:40 +0100

    navbar add

--- pathe-angu/src/app/app.module.ts
+++ pathe-angu/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ pathe-angu/src/app/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  pseudo: string | null = null;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // Récupérer le pseudo depuis le localStorage
+    this.pseudo = localStorage.getItem('pseudo');
+  }
+
+  logout() {
+    // Supprimer les données du localStorage
+    localStorage.removeItem('pseudo');
+    localStorage.removeItem('userId'); // Si d'autres données sont à supprimer, ajoute-les ici
+    alert('Déconnexion réussie');
+    // Rediriger vers la page de connexion
+    this.router.navigate(['/login']);
+  }
+}
